bbabf8b2e6020c9bc4974144a2570cbf4daadfb3
Peter <pcrall04@gmail.com>
2024-12-09 14:53:07 -0800

    Initial commit

---
+++ src/client/java/com/peter/claims/ClaimsClient.java
@@ -0,0 +1,10 @@
+package com.peter.claims;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClaimsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
